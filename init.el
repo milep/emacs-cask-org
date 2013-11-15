e6a68808a8a4b52f33753dc4ffa2851f7820c7c0
Miika Leppänen <maleppanen@gmail.com>
2013-11-15 02:51:24 -0800

    Switch to emacs snapshot

--- init.el
+++ init.el
@@ -1,25 +1,16 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
+(require 'pallet)
 
-(require 'package)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+;(require 'package)
+;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
-(dolist (package '(org pallet))
-  (unless (package-installed-p package)
-    (package-install package)))
-
-;; not working?
-;;(when (not (cl-remove-if-not 
-;;            (lambda (p) (equal 'org (car p)))
-;;            package-alist))
-;;  (message "No org-mode package found; installing now...")
-;;  (package-install 'org))
-
+;; Install the latest version of org manually
 (require 'org)
 (when (string-match "^[1234567]" (org-version))
   (warn "Org-Mode is out of date. We expect org 8 or higher, but instead we have %s" (org-version)))
 
-(require 'pallet)
 
 (org-babel-load-file "~/.emacs.d/emacs.org")
+
 
